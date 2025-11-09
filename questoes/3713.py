ca94b1dac8c10dba1d34b7e6a50ca96d6cff537b
AlexandreLJr <ubc183@gmail.com>
2025-11-09 20:58:10 -0300

    add questao 3713

---
+++ questoes/3713.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+
+        for i in range(n):
+            freq = {}
+            for j in range(i, n):
+                freq[s[j]] = freq.get(s[j], 0) + 1
+
+                values = list(freq.values())
+                if min(values) == max(values):
+                    ans = max(ans, j - i + 1)
+
+        return ans
